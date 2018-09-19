3dad4d80e3bc659ef297fd578abfc8b4ee8489df
Tiler Smith <TilerSmith@MacBook-Air.local>
2018-09-19 15:51:30 -0600

    added comments

--- src/Components/RP/Map.js
+++ src/Components/RP/Map.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {loadModules, loadCss} from 'esri-loader'
 
+
+//css strictly for the way the map views
 loadCss('https://js.arcgis.com/4.8/esri/css/main.css');
 
 const styles = {
@@ -16,27 +18,34 @@ height: '100%',
 
 class Map extends Component {
 	componentDidMount() {
+		//add any widgets or necessary features for the map.
     loadModules(['esri/Map', 'esri/views/MapView', 'esri/widgets/Locate']).then(([Map, MapView, Locate]) => {
+
+			//determines type of map
 
       const map = new Map({
         basemap: 'streets-navigation-vector'
-      });
+			});
+			
+			//initial scale and map size
 
       const mapView = new MapView({
         container: 'mapDiv',
         map,
         zoom: 3
 			});
+
+			//find my location
 			
 			const locate = new Locate({
 				view: mapView,
 				useHeadingEnabled: false,
 				goToOverride: function(view, options) {
-					options.target.scale = 1500;
+					options.target.scale = 5000;
 					return view.goTo(options.target);
 				}
 			});
-		
+		//adds the button to the map
 			mapView.ui.add(locate, "top-left");
 
       this.setState({
@@ -50,6 +59,7 @@ class Map extends Component {
   render() {
     return (
       <div>
+				{/* sets the size for the map(will need to change to be dynamic for each view) */}
         <div id="mapDiv" style={{height: 'calc(100vh - 190px)'}}>
         </div>
       </div>
