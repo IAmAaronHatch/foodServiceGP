7b79ab7b6277f14b5864f486195b22ea10c0b281
Aaron Hatch <aaron@Aarons-MacBook-Pro.local>
2018-09-24 10:52:45 -0600

    randombtn

--- src/Components/Reuse/RandomBtn.js
+++ src/Components/Reuse/RandomBtn.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { getRestaurants } from '../../Redux/reducers/rest';
 class RandomBtn extends Component {
-randomize = (arr) => {
-    // let mapped = arr.map(x => x.sort(function (a, b) { return 0.5 - Math.random()}))
-    let sorted = arr.sort(function(a, b){return 0.5 - Math.random()})
-    return sorted.splice(4, 15)
-}
+    handleRestaurants = (userLat, userLon) => {
+        //axios.get('yoururl').then(response => {
+        //  let randomRest = this.randomize(data)
+        //  Dispatch your action creator to update redux
+        // })
+    }
+    randomize = (arr) => {
+        let sorted = arr.sort(function (a, b) { return 0.5 - Math.random() })
+        return sorted.splice(4, 15)
+    }
 
     render() {
         console.log('props.rest', this.props.rest)
         return (
-            <div>   
+            <div>
                 <button onClick={() => this.randomize(this.props.rest.restaurants)}>Randomize!</button>
-                <button onClick={() => console.log(this.props.rest)}>Console log</button>       
+                <button onClick={() => console.log(this.props.rest)}>Console log</button>
             </div>
         )
     }
@@ -22,8 +28,10 @@ randomize = (arr) => {
 
 let mapStateToProps = state => {
     return {
-        rest: state.rest.data
+        rest: state.rest.data,
+        userLat: state.user.userLat,
+        userLon: state.user.userLon
     }
 }
 
-export default connect(mapStateToProps)(RandomBtn)
+export default connect(mapStateToProps, { getRestaurants })(RandomBtn)
